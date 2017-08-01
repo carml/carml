1ff38452684b7a77b2ec7d9dd4726c136d235b8a
Victor Louwerse <victor.louwerse@taxonic.com>
2017-08-01 14:41:35 +0200

    Cleanup unit test

--- rml-engine/src/test/java/com/taxonic/rdf_mapper/TestRdfMapper.java
+++ rml-engine/src/test/java/com/taxonic/rdf_mapper/TestRdfMapper.java
@@ -18,106 +18,110 @@ import com.taxonic.rdf_mapper.impl.MapperImpl;
 import com.taxonic.rml.model.TriplesMap;
 import com.taxonic.rml.model.impl.LogicalSourceImpl;
 import com.taxonic.rml.model.impl.ObjectMapImpl;
+import com.taxonic.rml.model.impl.PredicateMapImpl;
 import com.taxonic.rml.model.impl.PredicateObjectMapImpl;
 import com.taxonic.rml.model.impl.SubjectMapImpl;
 import com.taxonic.rml.model.impl.TriplesMapImpl;
 import com.taxonic.rml.util.IoUtils;
+import com.taxonic.rml.vocab.Rdf;
 
 public class TestRdfMapper {
 
-	private static final SimpleValueFactory f = SimpleValueFactory.getInstance();
+	static final SimpleValueFactory f = SimpleValueFactory.getInstance();
 	
-	private static class Rdf {
+	static class Example {
 		
-		// TODO not too happy about redefining the vocabs here. in pkg "vocab",
-		// they're defined as Strings. here, they're redefined in the RDF4J model API.
-		
-		static class Rml {
-			
-			static IRI iri(String suffix) { return f.createIRI(prefix + suffix); }
-			
-			static final String prefix = "http://semweb.mmlab.be/ns/rml#";
-			
-			static final IRI logicalSource = iri("logicalSource");
-			
+		static IRI iri(String suffix) {
+			return f.createIRI(prefix + suffix);
 		}
 		
-		static class Example {
-			
-			static IRI iri(String suffix) { return f.createIRI(prefix + suffix); }
-			
-			static final String prefix = "http://data.example.com/";
-			
-			static final IRI MyResource = iri("def/MyResource");
-			
-		}
+		static final String prefix = "http://data.example.com/";
+		
+		static final IRI
+			MyResource = iri("def/MyResource"),
+			when = iri("def/when"),
+			description = iri("def/description"),
+			accuracy = iri("def/accuracy");
+		
 	}
-	
+
 	@Test
 	public void test() {
 
-		// TODO use builder pattern
-		
 		List<TriplesMap> expected = Arrays.asList(
-			new TriplesMapImpl(
-				new LogicalSourceImpl(
-					"source-a.json",
-					"$",
-					f.createIRI("http://semweb.mmlab.be/ns/ql#JSONPath")
-				),
-				new SubjectMapImpl(
-					null,
-					null,
-					Rdf.Example.prefix + "resource/{id}",
-					null,
-					null,
-					set(Rdf.Example.MyResource)
-				),
-				set(
-					new PredicateObjectMapImpl(
-						set(), // TODO
-						set(
-							new ObjectMapImpl(
-								"when",
-								null,
-								null,
-								null,
-								null,
-								XMLSchema.DATE,
-								null
-							)
-						)
-					),
-					new PredicateObjectMapImpl(
-						set(), // TODO
-						set(
-							new ObjectMapImpl(
-								"description",
-								null,
-								null,
-								null,
-								null,
-								null,
-								null
-							)
-						)
-					),
-					new PredicateObjectMapImpl(
-						set(), // TODO
-						set(
-							new ObjectMapImpl(
-								"accuracy",
-								null,
-								null,
-								null,
-								null,
-								XMLSchema.FLOAT,
-								null
-							)
-						)
-					)
+			TriplesMapImpl.newBuilder()
+				.logicalSource(
+					LogicalSourceImpl.newBuilder()
+						.source("source-a.json")
+						.iterator("$")
+						.referenceFormulation(Rdf.Ql.JsonPath)
+						.build()
 				)
-			)
+				.subjectMap(
+					SubjectMapImpl.newBuilder()
+						.template(TestRdfMapper.Example.prefix + "resource/{id}")
+						.classes(set(TestRdfMapper.Example.MyResource))
+						.build()
+				)
+				.predicateObjectMap(
+					PredicateObjectMapImpl.newBuilder()
+						.predicateMap(
+							PredicateMapImpl.newBuilder()
+								.constant(TestRdfMapper.Example.when)
+								.build()
+						)
+						.objectMap(
+							ObjectMapImpl.newBuilder()
+								.reference("when")
+								.datatype(XMLSchema.DATE)
+								.build()
+						)
+						.build()
+				)
+				.predicateObjectMap(
+					PredicateObjectMapImpl.newBuilder()
+						.predicateMap(
+							PredicateMapImpl.newBuilder()
+								.constant(TestRdfMapper.Example.description)
+								.build()
+						)
+						.objectMap(
+							ObjectMapImpl.newBuilder()
+								.reference("description")
+								.build()
+						)
+						.build()
+				)
+				.predicateObjectMap(
+					PredicateObjectMapImpl.newBuilder()
+						.predicateMap(
+							PredicateMapImpl.newBuilder()
+								.constant(TestRdfMapper.Example.description)
+								.build()
+						)
+						.objectMap(
+							ObjectMapImpl.newBuilder()
+								.constant(f.createLiteral("constant description", "en"))
+								.build()
+						)
+						.build()
+				)
+				.predicateObjectMap(
+					PredicateObjectMapImpl.newBuilder()
+						.predicateMap(
+							PredicateMapImpl.newBuilder()
+								.constant(TestRdfMapper.Example.accuracy)
+								.build()
+						)
+						.objectMap(
+							ObjectMapImpl.newBuilder()
+								.reference("accuracy")
+								.datatype(XMLSchema.FLOAT)
+								.build()
+						)
+						.build()
+				)
+				.build()
 		);
 		
 		List<TriplesMap> result = loadTriplesMaps("test-a.rml.ttl");
