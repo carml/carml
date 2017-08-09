832998abe0a50734dfed754a63f0115d078a04e2
Victor Louwerse <victor.louwerse@taxonic.com>
2017-08-09 16:11:06 +0200

    Update RmlMapperTest

--- rml-engine/src/test/java/com/taxonic/rml/engine/RmlMapperTest.java
+++ rml-engine/src/test/java/com/taxonic/rml/engine/RmlMapperTest.java
@@ -1,13 +1,15 @@
 package com.taxonic.rml.engine;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.io.InputStream;
 import java.util.List;
+import java.util.function.Function;
 
 import org.eclipse.rdf4j.model.Model;
-import org.junit.Before;
 import org.junit.Test;
 
+import com.taxonic.rml.engine.template.TemplateParser;
 import com.taxonic.rml.model.TriplesMap;
 import com.taxonic.rml.util.IoUtils;
 import com.taxonic.rml.util.RmlMappingLoader;
@@ -18,23 +20,18 @@ public class RmlMapperTest {
 
 	private RmlMappingLoader loader = RmlMappingLoader.build();
 	
-	private RmlMapper mapper;
-	
-	// create a fresh mapper for every @Test
-	@Before
-	public void createMapper() {
-		mapper = new RmlMapper();
-	}
-	
 	@Test
 	public void testConstantSubjectShortcutMapping() {
-		testMapping("test1/constantSubjectShortcutMapping");
+		testMapping("RdfMapper/test1", "constantObjectShortcutMapping");
 	}
 	
-	private void testMapping(String resourceBaseName) {
-		List<TriplesMap> mapping = loader.load(resourceBaseName + ".rml.ttl");
+	private void testMapping(String resourcePath, String resourceBaseName) {
+		List<TriplesMap> mapping = loader.load(resourcePath + "/" + resourceBaseName + ".rml.ttl");
+		Function<String, InputStream> sourceResolver =
+			s -> RmlMapperTest.class.getClassLoader().getResourceAsStream(resourcePath + "/" + s);
+		RmlMapper mapper = new RmlMapper(sourceResolver, TemplateParser.build());
 		Model result = mapper.map(mapping);
-		Model expected = IoUtils.parse(resourceBaseName + ".output.ttl");
+		Model expected = IoUtils.parse(resourcePath + "/" + resourceBaseName + ".output.ttl");
 		assertEquals(expected, result);
 	}
 
