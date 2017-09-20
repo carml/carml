8561c44d88d365e64a869f0604be718dbe406d98
kaesve <mail@kaesve.nl>
2017-09-20 16:51:01 +0200

    Clean up subject mapper test and conform more to other tests in the project

--- carml-engine/src/test/java/com/taxonic/carml/engine/SubjectMapperTest.java
+++ carml-engine/src/test/java/com/taxonic/carml/engine/SubjectMapperTest.java
@@ -3,66 +3,77 @@ package com.taxonic.carml.engine;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Optional;
 
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.Resource;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.model.util.ModelBuilder;
 import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
-import com.taxonic.carml.model.SubjectMap;
-import com.taxonic.carml.model.impl.SubjectMapImpl;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SubjectMapperTest {
 
-	@Test
-	public void mapCallsEveryPredicateObjectMapper() throws Exception {
-		PredicateObjectMapper mockMapper1 = Mockito.mock(PredicateObjectMapper.class);
-		PredicateObjectMapper mockMapper2 = Mockito.mock(PredicateObjectMapper.class);
-		PredicateObjectMapper mockMapper3 = Mockito.mock(PredicateObjectMapper.class);
-		PredicateObjectMapper mockMapper4 = Mockito.mock(PredicateObjectMapper.class);
+	@Rule
+	public MockitoRule mockitoRule = MockitoJUnit.rule();
 
+	@Mock
+	TermGenerator<Resource> subjectGenerator;
+
+	@Test
+	public void map_withPredicateObjectMappers_callsEveryPredicateObjectMapper() throws Exception {
 		SimpleValueFactory f = SimpleValueFactory.getInstance();
 		IRI subjectIRI = f.createIRI("http://foo.bar/subjectIRI");
 
-		SubjectMap subjectMap = SubjectMapImpl.newBuilder().constant(subjectIRI).build();
-		TermGeneratorCreator tgc = TermGeneratorCreator.create(null);
-		TermGenerator subjectGenerator = tgc.getSubjectGenerator(subjectMap);
-
 		Model m = new ModelBuilder().build();
 		EvaluateExpression evaluator = null;
+
+		when(subjectGenerator.apply(evaluator))
+				.thenReturn(Optional.of(subjectIRI));
+
+		PredicateObjectMapper mockMapper1 = mock(PredicateObjectMapper.class);
+		PredicateObjectMapper mockMapper2 = mock(PredicateObjectMapper.class);
+		PredicateObjectMapper mockMapper3 = mock(PredicateObjectMapper.class);
+		PredicateObjectMapper mockMapper4 = mock(PredicateObjectMapper.class);
+
 
 		SubjectMapper s = new SubjectMapper(subjectGenerator, Collections.emptyList(), Collections.emptySet(),
 				Arrays.asList(mockMapper1, mockMapper2, mockMapper3, mockMapper4));
 
 		s.map(m, evaluator);
 
-		Mockito.verify(mockMapper1).map(Mockito.eq(m), Mockito.eq(evaluator), Mockito.any(), Mockito.eq(Collections.emptyList()));
-		Mockito.verify(mockMapper2).map(Mockito.eq(m), Mockito.eq(evaluator), Mockito.any(), Mockito.eq(Collections.emptyList()));
-		Mockito.verify(mockMapper3).map(Mockito.eq(m), Mockito.eq(evaluator), Mockito.any(), Mockito.eq(Collections.emptyList()));
-		Mockito.verify(mockMapper4).map(Mockito.eq(m), Mockito.eq(evaluator), Mockito.any(), Mockito.eq(Collections.emptyList()));
+		Mockito.verify(mockMapper1).map(m, evaluator, subjectIRI, Collections.emptyList());
+		Mockito.verify(mockMapper2).map(m, evaluator, subjectIRI, Collections.emptyList());
+		Mockito.verify(mockMapper3).map(m, evaluator, subjectIRI, Collections.emptyList());
+		Mockito.verify(mockMapper4).map(m, evaluator, subjectIRI, Collections.emptyList());
 	}
 
 	@Test
-	public void mapAppliesEveryClass() throws Exception {
+	public void map_withClasses_appliesEveryClassToGeneratedSubject() throws Exception {
 
 		SimpleValueFactory f = SimpleValueFactory.getInstance();
-		IRI subjectIRI = f.createIRI("https://www.none.invalid/subjectIRI");
+		IRI subjectIRI = f.createIRI("http://foo.bar/subjectIRI");
 
-		SubjectMap subjectMap = SubjectMapImpl.newBuilder().constant(subjectIRI).build();
-		TermGeneratorCreator tgc = TermGeneratorCreator.create(null);
-		TermGenerator subjectGenerator = tgc.getSubjectGenerator(subjectMap);
+		Model m = new ModelBuilder().build();
+		EvaluateExpression evaluator = null;
+
+		when(subjectGenerator.apply(evaluator))
+				.thenReturn(Optional.of(subjectIRI));
 
 		HashSet<IRI> expectedClasses = new HashSet<>();
 		expectedClasses.add(f.createIRI("http://www.none.invalid/foo"));
 		expectedClasses.add(f.createIRI("http://www.none.invalid/bar"));
 		expectedClasses.add(f.createIRI("http://www.none.invalid/baz"));
-
-		Model m = new ModelBuilder().build();
-		EvaluateExpression evaluator = null;
 
 		SubjectMapper s = new SubjectMapper(subjectGenerator, Collections.emptyList(), expectedClasses, Collections.emptyList());
 
