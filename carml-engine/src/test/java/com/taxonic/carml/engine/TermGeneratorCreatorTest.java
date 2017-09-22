12731ebb7aae5d65ddcea63004720990d3071110
kaesve <mail@kaesve.nl>
2017-09-22 13:22:15 +0200

    more unit tests for IRI generators

--- carml-engine/src/test/java/com/taxonic/carml/engine/TermGeneratorCreatorTest.java
+++ carml-engine/src/test/java/com/taxonic/carml/engine/TermGeneratorCreatorTest.java
@@ -4,14 +4,17 @@ import com.taxonic.carml.engine.template.TemplateParser;
 import com.taxonic.carml.model.TermType;
 import com.taxonic.carml.model.impl.ObjectMapImpl;
 
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import java.util.function.Function;
+import java.util.Optional;
 
-import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 public class TermGeneratorCreatorTest {
 
@@ -33,4 +36,74 @@ public class TermGeneratorCreatorTest {
 		Assert.assertNotNull(exception);
 		Assert.assertTrue(exception.getMessage().startsWith("2 generators were created for map"));
 	}
+
+	@Test
+	public void IriTermGenerator_withRelativeIRI_usesBaseIRI() throws Exception {
+
+		ValueFactory f = SimpleValueFactory.getInstance();
+
+		String baseIri = "http://base.iri";
+		TermGeneratorCreator tgc = new TermGeneratorCreator(f, baseIri, null, TemplateParser.build(), null);
+
+		String ref = "ref";
+		TermGenerator<Value> generator = tgc.getObjectGenerator(new ObjectMapImpl(ref, null, null, TermType.IRI, null, null, null, null));
+
+		String relativeIriPart = "/relativeIriPortion";
+		EvaluateExpression evaluator = Mockito.mock(EvaluateExpression.class);
+		when(evaluator.apply(ref)).thenReturn(Optional.of(relativeIriPart));
+		Optional<Value> result = generator.apply(evaluator);
+
+		Assert.assertTrue(result.isPresent());
+		Assert.assertTrue(result.get() instanceof IRI);
+		Assert.assertEquals(result.get(), f.createIRI(baseIri + relativeIriPart));
+	}
+
+	@Test
+	public void IriTermGenerator_withAbsoluteIRI_usesBaseIRI() throws Exception {
+
+		ValueFactory f = SimpleValueFactory.getInstance();
+
+		String baseIri = "http://base.iri";
+		TermGeneratorCreator tgc = new TermGeneratorCreator(f, baseIri, null, TemplateParser.build(), null);
+
+		String ref = "ref";
+		TermGenerator<Value> generator = tgc.getObjectGenerator(new ObjectMapImpl(ref, null, null, TermType.IRI, null, null, null, null));
+
+		String absoluteIri = "http://foo.bar/absoluteIri";
+		EvaluateExpression evaluator = Mockito.mock(EvaluateExpression.class);
+		when(evaluator.apply(ref)).thenReturn(Optional.of(absoluteIri));
+		Optional<Value> result = generator.apply(evaluator);
+
+		Assert.assertTrue(result.isPresent());
+		Assert.assertTrue(result.get() instanceof IRI);
+		Assert.assertEquals(result.get(), f.createIRI(absoluteIri));
+	}
+
+	@Test
+	public void IriTermGenerator_withFaultyIRI_throwsException() throws Exception {
+
+		ValueFactory f = SimpleValueFactory.getInstance();
+
+		String baseIri = "?";
+		TermGeneratorCreator tgc = new TermGeneratorCreator(f, baseIri, null, TemplateParser.build(), null);
+
+		String ref = "ref";
+		TermGenerator<Value> generator = tgc.getObjectGenerator(new ObjectMapImpl(ref, null, null, TermType.IRI, null, null, null, null));
+
+		String relativeIriPart = "/relativeIriPortion";
+		EvaluateExpression evaluator = Mockito.mock(EvaluateExpression.class);
+		when(evaluator.apply(ref)).thenReturn(Optional.of(relativeIriPart));
+
+		RuntimeException exception = null;
+		try {
+			generator.apply(evaluator);
+			Assert.assertTrue("This code should be unreachable", false);
+		} catch (RuntimeException e) {
+			exception = e;
+		}
+
+		Assert.assertNotNull(exception);
+		Assert.assertTrue(exception.getMessage().startsWith("data error: could not generate a valid iri"));
+	}
+
 }
