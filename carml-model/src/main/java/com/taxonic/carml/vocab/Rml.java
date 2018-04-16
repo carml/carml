457e3c81efc31de82024488cacece6bc17163459
joost aafjes <joostaafjes@gmail.com>
2018-04-16 12:20:47 +0200

    add TriplesMap to vocabulary

--- carml-model/src/main/java/com/taxonic/carml/vocab/Rml.java
+++ carml-model/src/main/java/com/taxonic/carml/vocab/Rml.java
@@ -7,10 +7,11 @@ public class Rml {
 		prefix = "http://semweb.mmlab.be/ns/rml#",
 		
 		BaseSource = prefix + "BaseSource",
+		TriplesMap = prefix + "TriplesMap",
 		referenceFormulation = prefix + "referenceFormulation",
 		source = prefix + "source",
 		reference = prefix + "reference",
 		iterator = prefix + "iterator",
 		logicalSource = prefix + "logicalSource";
-	
+
 }
