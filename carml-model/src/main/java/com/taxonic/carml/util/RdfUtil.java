aada64cb307471a1c184ba8708f6bae478b2f102
Pano Maria <pano.maria@gmail.com>
2020-08-10 10:03:36 +0200

    add and use iri validation util (#90)
    * add and use iri validation util
    
    * close class

---
+++ carml-model/src/main/java/com/taxonic/carml/util/RdfUtil.java
@@ -0,0 +1,22 @@
+package com.taxonic.carml.util;
+
+import org.eclipse.rdf4j.common.net.ParsedIRI;
+
+import java.net.URISyntaxException;
+
+public class RdfUtil {
+
+    private RdfUtil() {}
+
+    public static boolean isValidIri(String str) {
+        if (!str.contains(":")) {
+            return false;
+        }
+        try {
+            return new ParsedIRI(str).getScheme() != null;
+        } catch (URISyntaxException uriException) {
+            return false;
+        }
+    }
+
+}
