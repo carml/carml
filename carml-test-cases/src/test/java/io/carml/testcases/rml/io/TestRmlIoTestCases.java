fc486e84332ead756e34ce6956bef49a15b3ca74
Pano Maria <pano.maria@gmail.com>
2024-11-02 14:32:48 +0100

    update io test cases

--- carml-test-cases/src/test/java/io/carml/testcases/rml/io/TestRmlIoTestCases.java
+++ carml-test-cases/src/test/java/io/carml/testcases/rml/io/TestRmlIoTestCases.java
@@ -60,9 +60,8 @@ class TestRmlIoTestCases {
 
     private static final List<String> SKIP_TESTS = List.of(
             "RMLSTC0006e", // TODO: support https://www.w3.org/2019/wot/
-            "RMLSTC0006f", // https://github.com/kg-construct/rml-io/issues/78
+            // "RMLSTC0006f", // https://github.com/kg-construct/rml-io/issues/78
             "RMLSTC0007b", // TODO: raise issue, delimiter in mapping incorrect
-            "RMLSTC0007c", // TODO: raise issue, iterator has trailing slash and subject map missing @ for attribute
             "RMLSTC0007d" // TODO: add support for new XPath reference formulation, TODO: raise issue, iterator has
             // trailing slash and subject map missing @ for attribute
             );
